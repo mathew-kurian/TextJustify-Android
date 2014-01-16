824226cc1d477410bd3977c2429b32ace6736f02
Mathew Kurian <bluejamesbond@gmail.com>
2014-01-16 08:39:20 -0600

    Enable and disable justify

--- src/TextViewEx.java
+++ src/TextViewEx.java
@@ -29,150 +29,166 @@ import android.util.AttributeSet;
  */
 
 public class TextViewEx extends TextView 
-{		
-	private Paint paint = new Paint();
+{       
+    private Paint paint = new Paint();
 
-	private String [] blocks;
-	private float spaceOffset = 0;
-	private float horizontalOffset = 0;
-	private float verticalOffset = 0;
-	private float horizontalFontOffset = 0;
-	private float dirtyRegionWidth = 0;
-	
-	private float strecthOffset;
-	private float wrappedEdgeSpace;
-	private String block;
-	private String wrappedLine;
-	private String [] lineAsWords;
-	private Object[] wrappedObj;
-	
-	private Bitmap cache = null;
-	private boolean cacheEnabled = false;
-	
-	public TextViewEx(Context context, AttributeSet attrs, int defStyle) 
-	{
-		super(context, attrs, defStyle);
-	}
+    private String [] blocks;
+    private float spaceOffset = 0;
+    private float horizontalOffset = 0;
+    private float verticalOffset = 0;
+    private float horizontalFontOffset = 0;
+    private float dirtyRegionWidth = 0;
+    private boolean wrapEnabled = false;
+    
+    private float strecthOffset;
+    private float wrappedEdgeSpace;
+    private String block;
+    private String wrappedLine;
+    private String [] lineAsWords;
+    private Object[] wrappedObj;
+    
+    private Bitmap cache = null;
+    private boolean cacheEnabled = false;
+    
+    public TextViewEx(Context context, AttributeSet attrs, int defStyle) 
+    {
+        super(context, attrs, defStyle);
+    }
 
-	public TextViewEx(Context context, AttributeSet attrs) 
-	{
-		super(context, attrs);
-	}
+    public TextViewEx(Context context, AttributeSet attrs) 
+    {
+        super(context, attrs);
+    }
 
-	public TextViewEx(Context context) 
-	{
-		super(context);
-	}
-	
-	@Override
-	public void setDrawingCacheEnabled(boolean cacheEnabled) {
-		this.cacheEnabled = cacheEnabled;
-	}
+    public TextViewEx(Context context) 
+    {
+        super(context);
+    }
+    
+    @Override
+    public void setDrawingCacheEnabled(boolean cacheEnabled) 
+    {
+        this.cacheEnabled = cacheEnabled;
+    }
 
-	@Override
-	protected void onDraw(Canvas canvas) 
-	{
-		// Active canas needs to be set
-		// based on cacheEnabled
-		Canvas activeCanvas = null;
+    public void setText(String st, boolean wrap)
+    {
+        wrapEnabled = wrap;
+        super.setText(st);
+    }
 
-		// Set the active canvas based on
-		// whether cache is enabled
-		if (cacheEnabled) {
+    @Override
+    protected void onDraw(Canvas canvas) 
+    { 
+        // If wrap is disabled then,
+        // request original onDraw
+        if(!wrapEnabled)
+        {
+            super.onDraw(canvas);
+            return;
+        }
+        
+        // Active canas needs to be set
+        // based on cacheEnabled
+        Canvas activeCanvas = null;
 
-			if (cache != null) 
-			{
-				// Draw to the OS provided canvas
-				// if the cache is not empty
-				canvas.drawBitmap(cache, 0, 0, paint);
-				return;
-			} 
-			else  
-			{
-				// Create a bitmap and set the activeCanvas
-				// to the one derived from the bitmap
-				cache = Bitmap.createBitmap(getWidth(), getHeight(), 
-	                    Config.ARGB_4444);
-				activeCanvas = new Canvas(cache);
-			}
-		} 
-		else 
-		{
-			// Active canvas is the OS
-			// provided canvas
-			activeCanvas = canvas;
-		}
-		
-		// Pull widget properties
-		paint.setColor(getCurrentTextColor());
-		paint.setTypeface(getTypeface());
-		paint.setTextSize(getTextSize());
-				
-		dirtyRegionWidth = getWidth();
-		int maxLines = getMaxLines(), lines = 1;
-		blocks = getText().toString().split("((?<=\n)|(?=\n))");
-		verticalOffset = horizontalFontOffset = getLineHeight() - 0.5f; // Temp fix
-		spaceOffset = paint.measureText(" ");	
+        // Set the active canvas based on
+        // whether cache is enabled
+        if (cacheEnabled) {
 
-		for(int i = 0; i < blocks.length && lines <= maxLines; i++)
-		{
-			block = blocks[i];
-			horizontalOffset = 0;
+            if (cache != null) 
+            {
+                // Draw to the OS provided canvas
+                // if the cache is not empty
+                canvas.drawBitmap(cache, 0, 0, paint);
+                return;
+            } 
+            else  
+            {
+                // Create a bitmap and set the activeCanvas
+                // to the one derived from the bitmap
+                cache = Bitmap.createBitmap(getWidth(), getHeight(), 
+                        Config.ARGB_4444);
+                activeCanvas = new Canvas(cache);
+            }
+        } 
+        else 
+        {
+            // Active canvas is the OS
+            // provided canvas
+            activeCanvas = canvas;
+        }
+        
+        // Pull widget properties
+        paint.setColor(getCurrentTextColor());
+        paint.setTypeface(getTypeface());
+        paint.setTextSize(getTextSize());
+                
+        dirtyRegionWidth = getWidth();
+        int maxLines = getMaxLines(), lines = 1;
+        blocks = getText().toString().split("((?<=\n)|(?=\n))");
+        verticalOffset = horizontalFontOffset = getLineHeight() - 0.5f; // Temp fix
+        spaceOffset = paint.measureText(" ");   
 
-			if(block.length() == 0)
-			{
-				continue;
-			}			
-			else if(block.equals("\n"))
-			{
-				verticalOffset += horizontalFontOffset; 
-				continue;
-			}
-			
-			block = block.trim();
-			
-			if(block.length() == 0) 
-			{
-				continue;
-			}
-			
-			wrappedObj = TextJustifyUtils.createWrappedLine(block, paint, spaceOffset, dirtyRegionWidth);
-			
-			wrappedLine = ((String) wrappedObj[0]);
-			wrappedEdgeSpace = (Float) wrappedObj[1];
-			lineAsWords = wrappedLine.split(" ");
-			strecthOffset = wrappedEdgeSpace != Float.MIN_VALUE ? wrappedEdgeSpace/(lineAsWords.length - 1) : 0;
-			
-			for(int j = 0; j < lineAsWords.length; j++)
-			{
-				String word = lineAsWords[j];
-				if (lines == maxLines && j == lineAsWords.length - 1) 
-				{
-					activeCanvas.drawText("...", horizontalOffset, verticalOffset, paint);
-				} 
-				else 
-				{
-					activeCanvas.drawText(word, horizontalOffset, verticalOffset, paint);
-				}
+        for(int i = 0; i < blocks.length && lines <= maxLines; i++)
+        {
+            block = blocks[i];
+            horizontalOffset = 0;
 
-				horizontalOffset += paint.measureText(word) + spaceOffset + strecthOffset;
-			}
+            if(block.length() == 0)
+            {
+                continue;
+            }           
+            else if(block.equals("\n"))
+            {
+                verticalOffset += horizontalFontOffset; 
+                continue;
+            }
+            
+            block = block.trim();
+            
+            if(block.length() == 0) 
+            {
+                continue;
+            }
+            
+            wrappedObj = TextJustifyUtils.createWrappedLine(block, paint, spaceOffset, dirtyRegionWidth);
+            
+            wrappedLine = ((String) wrappedObj[0]);
+            wrappedEdgeSpace = (Float) wrappedObj[1];
+            lineAsWords = wrappedLine.split(" ");
+            strecthOffset = wrappedEdgeSpace != Float.MIN_VALUE ? wrappedEdgeSpace/(lineAsWords.length - 1) : 0;
+            
+            for(int j = 0; j < lineAsWords.length; j++)
+            {
+                String word = lineAsWords[j];
+                if (lines == maxLines && j == lineAsWords.length - 1) 
+                {
+                    activeCanvas.drawText("...", horizontalOffset, verticalOffset, paint);
+                } 
+                else 
+                {
+                    activeCanvas.drawText(word, horizontalOffset, verticalOffset, paint);
+                }
 
-			lines++;
-			
-			if(blocks[i].length() > 0)
-			{
-				blocks[i] = blocks[i].substring(wrappedLine.length());				
-				verticalOffset += blocks[i].length() > 0 ? horizontalFontOffset : 0; 				
-				i--;
-			}
-		}
-		
-		if (cacheEnabled)
-		{
-			// Draw the cache onto the OS provided
-			// canvas.
-			canvas.drawBitmap(cache, 0, 0, paint);
-		}
-	}
+                horizontalOffset += paint.measureText(word) + spaceOffset + strecthOffset;
+            }
+
+            lines++;
+            
+            if(blocks[i].length() > 0)
+            {
+                blocks[i] = blocks[i].substring(wrappedLine.length());              
+                verticalOffset += blocks[i].length() > 0 ? horizontalFontOffset : 0;                
+                i--;
+            }
+        }
+        
+        if (cacheEnabled)
+        {
+            // Draw the cache onto the OS provided
+            // canvas.
+            canvas.drawBitmap(cache, 0, 0, paint);
+        }
+    }
 }

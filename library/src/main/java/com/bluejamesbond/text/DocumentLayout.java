017d80f8c72e1779c29c11ea81d9a97f332bd1a2
Mathew Kurian <bluejamesbond@gmail.com>
2015-01-09 03:44:48 -0600

    Adding getMaxTextureSize

--- library/src/main/java/com/bluejamesbond/text/DocumentLayout.java
+++ library/src/main/java/com/bluejamesbond/text/DocumentLayout.java
@@ -35,7 +35,6 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
-import android.opengl.GLES10;
 import android.text.TextPaint;
 
 import com.bluejamesbond.text.hyphen.Hyphenator;
@@ -46,12 +45,14 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.ListIterator;
 
-import javax.microedition.khronos.opengles.GL10;
+import javax.microedition.khronos.egl.EGL10;
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.egl.EGLContext;
+import javax.microedition.khronos.egl.EGLDisplay;
 
 @SuppressWarnings("unused")
 public class DocumentLayout {
 
-    public static final int GL_MAX_TEXTURE_SIZE = 4096;
     private static Integer bitmapHeight = -1;
 
     // Debugging
@@ -76,7 +77,7 @@ public class DocumentLayout {
 
         synchronized (bitmapHeight) {
             if (DocumentLayout.bitmapHeight.equals(-1)) {
-                DocumentLayout.bitmapHeight = Math.min(context.getResources().getDisplayMetrics().heightPixels * 7 / 6, GL_MAX_TEXTURE_SIZE);
+                DocumentLayout.bitmapHeight = Math.min(context.getResources().getDisplayMetrics().heightPixels * 7 / 6, getMaxTextureSize());
             }
         }
 
@@ -92,6 +93,49 @@ public class DocumentLayout {
 
         mTokens = new Token[0];
         chunks = new ConcurrentModifiableLinkedList<String>();
+    }
+
+    private static int getMaxTextureSize() {
+        // Code from
+        // http://stackoverflow.com/a/26823209/1100536
+
+        // Safe minimum default size
+        final int GL_MAX_TEXTURE_SIZE = 2048;
+
+        // Get EGL Display
+        EGL10 egl = (EGL10) EGLContext.getEGL();
+        EGLDisplay display = egl.eglGetDisplay(EGL10.EGL_DEFAULT_DISPLAY);
+
+        // Initialise
+        int[] version = new int[2];
+        egl.eglInitialize(display, version);
+
+        // Query total number of configurations
+        int[] totalConfigurations = new int[1];
+        egl.eglGetConfigs(display, null, 0, totalConfigurations);
+
+        // Query actual list configurations
+        EGLConfig[] configurationsList = new EGLConfig[totalConfigurations[0]];
+        egl.eglGetConfigs(display, configurationsList, totalConfigurations[0], totalConfigurations);
+
+        int[] textureSize = new int[1];
+        int maximumTextureSize = 0;
+
+        // Iterate through all the configurations to located the maximum texture size
+        for (int i = 0; i < totalConfigurations[0]; i++) {
+            // Only need to check for width since opengl textures are always squared
+            egl.eglGetConfigAttrib(display, configurationsList[i], EGL10.EGL_MAX_PBUFFER_WIDTH, textureSize);
+
+            // Keep track of the maximum texture size
+            if (maximumTextureSize < textureSize[0])
+                maximumTextureSize = textureSize[0];
+        }
+
+        // Release
+        egl.eglTerminate(display);
+
+        // Return largest texture size found, or default
+        return Math.max(maximumTextureSize, GL_MAX_TEXTURE_SIZE);
     }
 
     public boolean isDebugging() {
